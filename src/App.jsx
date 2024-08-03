6b59a461c7b3b928d5c7de34c0ebe6604176e43e
sree-r-one <sreeraj_ec@hotmail.com>
2024-08-04 04:42:28 +0800

    home completed

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,8 @@ const App = () => {
       <Navbar />
       <div className="mx-auto mt-20 max-w-6xl space-y-12 px-4">
         <Home />
-        <Footer />
-        <Footer />
-        <Footer />
-        <Footer />
-        <Footer />
-        <Footer />
       </div>
+      <Footer />
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,6 +29,73 @@ const Hero = () => {
   );
 };
 
+const Timeline = () => {
+  return (
+    <ol class="relative border-s border-gray-200 bg-white dark:border-gray-700">
+      <li class="mb-10 ms-4">
+        <div class="bg-brightRed absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+          February 2022
+        </time>
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+          Application UI code in Tailwind CSS
+        </h3>
+        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          Get access to over 20+ pages including a dashboard layout, charts,
+          kanban board, calendar, and pre-order E-commerce & Marketing pages.
+        </p>
+        <a
+          href="#"
+          class="inline-flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+        >
+          Learn more{" "}
+          <svg
+            class="ms-2 h-3 w-3 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </li>
+      <li class="mb-10 ms-4">
+        <div class="bg-brightRed absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+          March 2022
+        </time>
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+          Marketing UI design in Figma
+        </h3>
+        <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+          All of the pages and components are first designed in Figma and we
+          keep a parity between the two versions even as we update the project.
+        </p>
+      </li>
+      <li class="ms-4">
+        <div class="bg-brightRed absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+          April 2022
+        </time>
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+          E-Commerce UI code in Tailwind CSS
+        </h3>
+        <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+          Get started with dozens of web components and interactive elements
+          built on top of Tailwind CSS.
+        </p>
+      </li>
+    </ol>
+  );
+};
+
 const Jumbotron = () => {
   return (
     <section class="rounded-lg bg-white drop-shadow-md dark:bg-gray-900">
@@ -43,7 +110,7 @@ const Jumbotron = () => {
         <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
           <a
             href="#"
-            class="bg-brightRed inline-flex items-center justify-center rounded-lg px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+            class="bg-brightRed hover:bg-brightRedLight inline-flex items-center justify-center rounded-lg px-5 py-3 text-center text-base font-medium text-white focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
           >
             Get started
             <svg
@@ -80,6 +147,7 @@ const Home = () => {
 
       <Hero />
       <Jumbotron />
+      <Timeline />
     </div>
   );
 };
