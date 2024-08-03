9e6226d22e9e505c28a1117928d296cd62f22a77
sree-r-one <sreeraj_ec@hotmail.com>
2024-08-03 22:32:10 +0800

    added files before finality

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <section className="bg-brightRed shadow-md">
       {/* Flex Container */}
-      <div className="md:flex-row md:space-y-0 mx-auto mt-12 flex flex-col items-center justify-between space-y-12 px-6 py-12">
+      <div className="md:flex-row md:space-y-0 md:px-6 mx-auto mt-12 flex flex-col items-center justify-between space-y-12 px-10 py-12">
         <h2 className="md:text-left md:text-4xl md:max-w-xl text-center text-5xl font-bold text-white">
           Simplify how your team works today
         </h2>
